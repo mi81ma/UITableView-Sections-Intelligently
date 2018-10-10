caf6be2cbb1c217b7845d2d6b193716eaa0d1046
masato <masato@m-mbp.local>
2018-10-10 15:47:28 +0800

    commit

--- UITableView Sections Intelligently/AppDelegate.swift
+++ UITableView Sections Intelligently/AppDelegate.swift
@@ -15,7 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+
+        // MARK:- Add the Window Codes to "func application"
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+
+        let navController = UINavigationController(rootViewController: ViewController())
+
+        // set navContraller on window?.rootViewController
+        window?.rootViewController = navController
+
         return true
     }
 
